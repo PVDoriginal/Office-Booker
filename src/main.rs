ea2f7959a98805a3a91f12b7bc49d86e231a0e3f
Damon <pvdoriginal2019@gmail.com>
2025-11-08 13:07:49 +0200

    finished smarthack

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    Json, Router,
+    extract::Path,
+    http::StatusCode,
+    routing::{get, post},
+};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users/{user}", get(create_user));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn create_user(Path(user): Path<String>) -> StatusCode {
+    println!("who the fuck is here? {user}");
+    StatusCode::CREATED
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
 }
